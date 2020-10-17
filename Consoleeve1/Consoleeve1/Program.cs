bfd60a2b323b49a05e34181f1f3d1825b591d706
eve200 <evelymuza@gmail.com>
2020-10-17 19:52:38 +0200

    Fcoomi

---
+++ Consoleeve1/Consoleeve1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consoleeve1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<vehicle> lstvehicle = new List<vehicle>();
+            lstvehicle.Add(new vehicle());
+            lstvehicle[0].Vehicle_reg = "ACU";
+            lstvehicle[0].Owner_natid = 678999021;
+            lstvehicle[0].Amount = 100;
+            lstvehicle[0].time = Convert.ToDateTime("10/17/2020");
+
+            lstvehicle.Add(new vehicle());
+            lstvehicle[1].Vehicle_reg = "AEK";
+            lstvehicle[1].Owner_natid = 090987676;
+            lstvehicle[1].Amount = 100;
+            lstvehicle[1].time = Convert.ToDateTime("10/16/2020");
+
+            foreach (vehicle oStud in lstvehicle)
+            {
+                Console.WriteLine(oStud.getData());
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Consoleeve1/Consoleeve1/vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consoleeve1
+{
+    class vehicle
+    {
+        public string Vehicle_reg;
+        public double Owner_natid;
+        public double Amount;
+        public DateTime time;
+
+        public string getData()
+        {
+            return this.Vehicle_reg + " is owned by user with I.D " + this.Owner_natid + " pays amount " + this.Amount + "/month " + " registration day " + this.time;
+        }
+    }
+
+}
